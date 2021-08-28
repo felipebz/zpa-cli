94b04bcb48782ce62484dc619c12e6420bdad7b9
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2021-08-28 10:25:42 -0300

    Bump Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ version = "2.0.0-SNAPSHOT"
 
 plugins {
     `maven-publish`
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.5.30"
     application
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
