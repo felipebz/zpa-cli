9b3c0c6cbfc8a64753f15c039e53d85954771feb
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2021-10-17 21:48:19 -0300

    Simplify run-sonar-scanner example

--- source-test/run-sonar-scanner.sh
+++ source-test/run-sonar-scanner.sh
@@ -2,44 +2,25 @@
 
 set -euo pipefail
 
-ZPA_CLI_VERSION=2.0.0-SNAPSHOT
-SONAR_SCANNER_VERSION=4.5.0.2216
-TOOLS_PATH=`pwd`/source-test/tools
+docker run --rm \
+    -v $PWD/source-test/utPLSQL:/src \
+    felipebz/zpa-cli:nightly \
+    --sources . \
+    --output-file zpa-issues.json \
+    --output-format sq-generic-issue-import
 
-export LANG=en_US.UTF-8
-export PATH=$PATH:$TOOLS_PATH/sonar-scanner-$SONAR_SCANNER_VERSION/bin:$TOOLS_PATH/zpa-cli/bin
-
-mkdir -p $TOOLS_PATH
-cd $TOOLS_PATH
-
-# Extract sonar-scanner
-
-if [ ! -x "./sonar-scanner-$SONAR_SCANNER_VERSION/bin/sonar-scanner" ]; then
-  wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION.zip
-  unzip sonar-scanner-cli-$SONAR_SCANNER_VERSION.zip
-fi
-
-# Extract zpa-cli
-
-rm -rf zpa-cli/
-tar xvf ../../build/distributions/zpa-cli-$ZPA_CLI_VERSION.tar
-
-# Execute an analysis
-
-cd ../utPLSQL/
-
-$TOOLS_PATH/zpa-cli-$ZPA_CLI_VERSION/bin/zpa-cli --sources . --output-file zpa-issues.json --output-format sq-generic-issue-import
-sonar-scanner -Dsonar.projectKey=utPLSQL-zpa-demo \
+docker run --rm \
+    -e SONAR_HOST_URL="https://sonarcloud.io" \
+    -e SONAR_LOGIN=$SONARCLOUD_TOKEN \
+    -v $PWD/source-test/utPLSQL:/usr/src \
+    sonarsource/sonar-scanner-cli \
+    -Dsonar.projectKey=utPLSQL-zpa-demo \
     -Dsonar.organization=$SONARCLOUD_ORGANIZATION \
-    -Dsonar.host.url=https://sonarcloud.io \
-    -Dsonar.login=$SONARCLOUD_TOKEN \
     -Dsonar.externalIssuesReportPaths=zpa-issues.json \
     -Dsonar.pullrequest.provider= \
     -Dsonar.coverageReportPaths= \
     -Dsonar.testExecutionReportPaths= \
     -Dsonar.plsql.file.suffixes=sql,pkg,pks,pkb,fun,pcd,tgg,prc,tpb,trg,typ,tab,tps \
     -Dsonar.projectName="utPLSQL analyzed by ZPA" \
-    -Dsonar.sources=. \
-    -Dsonar.tests= \
-
-rm -rf zpa-issues.json .scannerwork/
+    -Dsonar.scm.disabled=true \
+    -Dsonar.tests=
