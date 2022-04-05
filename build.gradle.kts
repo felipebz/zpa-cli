0024655329ec2ab230fcfe3600700e05dc3acc38
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2022-04-04 22:12:53 -0300

    Use Kotlin 1.6.20 and Gradle 7.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ version = "2.0.0-SNAPSHOT"
 
 plugins {
     `maven-publish`
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.6.20"
     application
     id("com.github.johnrengelman.shadow") version "7.1.0"
 }
