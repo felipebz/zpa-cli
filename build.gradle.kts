45e2183ba0eee568848dec775344e230960b658d
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2022-04-21 19:15:20 -0300

    Update Kotlin to 1.6.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ version = "2.0.0-SNAPSHOT"
 
 plugins {
     `maven-publish`
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.6.21"
     application
     id("com.github.johnrengelman.shadow") version "7.1.0"
 }
