4cdbf0addb9c02fb67628ee89681e8bd3f79d37d
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2024-01-29 19:32:17 -0300

    build: Update to Gradle 8.6 and Kotlin 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ version = "2.0.0-SNAPSHOT"
 
 plugins {
     `maven-publish`
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.22"
     application
     id("org.jreleaser") version "1.8.0"
     id("org.jreleaser.jdks") version "1.8.0"
