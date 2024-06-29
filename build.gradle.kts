dd2194c24370f971fda815daa380afb063c4d7a9
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2024-06-28 23:40:18 -0300

    build: Bump project version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jreleaser.model.api.common.ArchiveOptions
 
 group = "com.felipebz.zpa"
-version = "2.0.0"
+version = "2.1.0-SNAPSHOT"
 
 plugins {
     `maven-publish`
