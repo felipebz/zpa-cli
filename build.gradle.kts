5ff7849648739b0752e16dd61fbed9e4ef9c9213
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2024-06-28 22:35:24 -0300

    build(deps): Use ZPA 3.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ repositories {
 dependencies {
     implementation("org.sonarsource.api.plugin:sonar-plugin-api:9.14.0.375")
     implementation("org.jcommander:jcommander:1.83")
-    implementation("com.felipebz.zpa:sonar-zpa-plugin:3.5.0-SNAPSHOT")
+    implementation("com.felipebz.zpa:sonar-zpa-plugin:3.5.1")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.pf4j:pf4j:3.11.1")
     implementation("org.slf4j:slf4j-jdk14:2.0.13")
