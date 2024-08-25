1b68723653c4c919621a5864885d23fd0461c4ca
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2024-08-25 19:40:15 -0300

    chore: Update Kotlin to 2.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ version = "2.1.0-SNAPSHOT"
 
 plugins {
     `maven-publish`
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.0.20"
     application
     id("org.jreleaser") version "1.13.1"
     id("org.jreleaser.jdks") version "1.13.1"
