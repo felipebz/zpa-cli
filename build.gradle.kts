6ff6f2725b3abea2a332e71cb36e0c4b2d5a4ab5
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2025-10-28 22:00:09 -0300

    build: release 3.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jreleaser.model.api.common.ArchiveOptions
 
 group = "com.felipebz.zpa"
-version = "3.1.0-SNAPSHOT"
+version = "3.0.1"
 
 plugins {
     `maven-publish`
