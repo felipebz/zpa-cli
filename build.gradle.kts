70b4f094932d20682e6472f045d58996727b3a3d
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2026-09-02 20:55:14 -0300

    build: prepare for 3.2.0-SNAPSHOT development

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jreleaser.model.api.common.ArchiveOptions
 
 group = "com.felipebz.zpa"
-version = "3.1.0"
+version = "3.2.0-SNAPSHOT"
 
 plugins {
     `maven-publish`
